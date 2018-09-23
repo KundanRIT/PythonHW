203295986daac6ecf48ca133a1ecaa2d6f11e795
Kundan Kumar <kk7272@rit.edu>
2018-09-22 20:10:07 -0400

    homework 3

--- polygons.py
+++ polygons.py
@@ -11,12 +11,13 @@ def recursion(side, length, fill, COLORS, vector):
         if fill:
             vector.begin_fill()
         for _ in range(side):
-            # vector.forward(length)
             vector.left(360 / side)
             vector.forward(length)
         if fill:
             vector.end_fill()
+        return side * length
     else:
+        distance = 0
         if fill:
             vector.begin_fill()
             for i in range(side):
@@ -27,9 +28,10 @@ def recursion(side, length, fill, COLORS, vector):
             vector.color(COLORS[side - 3])
             vector.forward(length)
             vector.right((360/(side-1))-(360/side))
-            recursion(side-1, length/2, fill, COLORS, vector)
+            distance += recursion(side-1, length/2, fill, COLORS, vector)
             vector.left((360/(side-1))-(360/side))
             vector.left(360/side)
+        return side*length + distance
 
 def main():
     randomcolors = [
@@ -40,13 +42,15 @@ def main():
         (random(),random(),random()),
         (random(),random(),random()),
     ]
-    tracer(False)
+    # tracer(False)
     vector = Turtle()
+    vector.speed(0)
     vector.penup()
     vector.goto(-200,-200)
     vector.pendown()
-    vector.pensize(8)
-    recursion(7,200,True,randomcolors,vector)
+    vector.pensize(2)
+    distance = recursion(6,200,False,randomcolors,vector)
+    print(distance)
     update()
     mainloop()
 
