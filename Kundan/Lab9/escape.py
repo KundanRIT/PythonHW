6e56684b0f09b7e094596e8a2f932ca6fc42f029
Kundan Kumar <kk7272@rit.edu>
2018-12-06 22:03:54 -0500

    homework 9

--- Kundan/Lab9/escape.py
+++ Kundan/Lab9/escape.py
@@ -72,53 +72,31 @@ def makeVertexName(x, y):
     return str(y) + "," + str(x)
 
 
-def main():
-    rowSize = 0
-    columnSize = 0
-    exitRow = -1
-    # pond = [[".",".","*",".","."], [".",".",".",".","."], [".",".",".",".","."], [".",".",".","*","."], [".","*",".",".","."]]
-    pond = []
-    with open("test1.txt") as f:
-        firstLineRead = False
-        for line in f:
-            if not firstLineRead:
-                rowSize, columnSize, exitRow = line.strip().split()
-                rowSize = int(rowSize)
-                columnSize = int(columnSize)
-                exitRow = int(exitRow)
-                firstLineRead = True
-            else:
-                pond.append(line.strip().split())
-    for x in range(0, rowSize):
-        for y in range(0, columnSize):
-            print(pond[x][y], end=" ")
-        print()
-    exitVertex = makeVertexName(exitRow, columnSize-1)
+def createGraph(pond, rowSize, columnSize):
     graph = Graph()
-    sources = []
     for x in range(0, rowSize):
         for y in range(0, columnSize):
             if pond[x][y] != "*":
                 src = makeVertexName(x, y)
-                sources.append(src)
+                graph.addVertex(src)
 
-                if y != columnSize-1:
+                if y != columnSize - 1:
                     # go right
                     rockIndex = -1
-                    for k in range(y+1, columnSize):
+                    for k in range(y + 1, columnSize):
                         if pond[x][k] == "*":
                             rockIndex = k
                             break
                     if rockIndex != -1:
-                        dest = makeVertexName(x, rockIndex-1)
+                        dest = makeVertexName(x, rockIndex - 1)
                     else:
-                        dest = makeVertexName(x, columnSize-1)
+                        dest = makeVertexName(x, columnSize - 1)
                     graph.addEdge(src, dest)
 
                 if y != 0:
                     # go left
                     rockIndex = -1
-                    for k in range(y-1, -1, -1):
+                    for k in range(y - 1, -1, -1):
                         if pond[x][k] == "*":
                             rockIndex = k
                             break
@@ -128,23 +106,23 @@ def main():
                         dest = makeVertexName(x, 0)
                     graph.addEdge(src, dest)
 
-                if x != rowSize-1:
+                if x != rowSize - 1:
                     # go down
                     rockIndex = -1
-                    for k in range(x+1, rowSize):
+                    for k in range(x + 1, rowSize):
                         if pond[k][y] == "*":
                             rockIndex = k
                             break
                     if rockIndex != -1:
-                        dest = makeVertexName(rockIndex-1, y)
+                        dest = makeVertexName(rockIndex - 1, y)
                     else:
-                        dest = makeVertexName(columnSize-1, y)
+                        dest = makeVertexName(columnSize - 1, y)
                     graph.addEdge(src, dest)
 
                 if x != 0:
                     # go up
                     rockIndex = -1
-                    for k in range(x-1, -1, -1):
+                    for k in range(x - 1, -1, -1):
                         if pond[k][y] == "*":
                             rockIndex = k
                             break
@@ -153,14 +131,17 @@ def main():
                     else:
                         dest = makeVertexName(0, y)
                     graph.addEdge(src, dest)
+    return graph
+
+
+def printExitPaths(graph, exitVertex):
     escapes = {}
-    escapes[-1] = []
-    for src in sources:
+    for src in graph.vertices.keys():
         if src == exitVertex:
             steps = 1
         else:
             steps = findShortestPath(graph.getVertex(src), graph.getVertex(
-            exitVertex))
+                exitVertex))
         if steps is None:
             key = -1
         else:
@@ -169,10 +150,40 @@ def main():
             escapes[key].append(src)
         except KeyError:
             escapes[key] = [src]
-    for key in range(0, graph.size):
+    for key in range(1, graph.size + 1):
         if key in escapes:
             print(key, " : ", escapes[key])
-    print("Not Reachable : ", escapes[-1])
+    if -1 in escapes:
+        print("Not Reachable : ", escapes[-1])
+
+
+def main():
+    filenames = ["test1.txt", "test2.txt", "test3.txt"]
+    for filename in filenames:
+        print("Testing", filename)
+        rowSize = 0
+        columnSize = 0
+        exitRow = -1
+        pond = []
+        with open(filename) as f:
+            firstLineRead = False
+            for line in f:
+                if not firstLineRead:
+                    rowSize, columnSize, exitRow = line.strip().split()
+                    rowSize = int(rowSize)
+                    columnSize = int(columnSize)
+                    exitRow = int(exitRow)
+                    firstLineRead = True
+                else:
+                    pond.append(line.strip().split())
+        for x in range(0, rowSize):
+            for y in range(0, columnSize):
+                print(pond[x][y], end=" ")
+            print()
+        graph = createGraph(pond, rowSize, columnSize)
+        exitVertex = makeVertexName(exitRow, columnSize-1)
+        printExitPaths(graph, exitVertex)
+        print("\n")
 
 if __name__ == '__main__':
     main()
